80fae49b390ef7a19318ff9b9175feb0b5804ba9
Israel Miller <ismille@microsoft.com>
2021-10-13 16:49:35 -0400

    idempotent install

--- install-osm.sh
+++ install-osm.sh
@@ -9,7 +9,9 @@ curl -sL "https://github.com/openservicemesh/osm/releases/download/$OSM_VERSION/
 sudo mv ./linux-amd64/osm /usr/local/bin/osm
 osm version
 
-OSM_INSTALL=$(osm install 2>&1 >/dev/null)
+set +e
+OSM_INSTALL=$(osm install 2>&1) || true
+set -e
 
 if grep -q "Error: Mesh osm already exists" <<< "$OSM_INSTALL"; then
     osm mesh upgrade
