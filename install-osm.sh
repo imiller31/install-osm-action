e02f30631bf9485f8d2a70c8ffcc51b1082972df
Nick Keller <nikelle@microsoft.com>
2021-10-15 15:14:11 -0400

    Add back in sudo

--- install-osm.sh
+++ install-osm.sh
@@ -6,7 +6,7 @@ OSM_VERSION=v0.11.0
 # Linux curl command only
 curl -sL "https://github.com/openservicemesh/osm/releases/download/$OSM_VERSION/osm-$OSM_VERSION-linux-amd64.tar.gz" | tar -vxzf -
 
-mv ./linux-amd64/osm /usr/local/bin/osm
+sudo mv ./linux-amd64/osm /usr/local/bin/osm
 osm version
 
 set +e
