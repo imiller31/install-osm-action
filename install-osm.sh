cd4c9d16d2fff003f4ab79392138989db8ef20f6
Nick Keller <nikelle@microsoft.com>
2021-10-15 15:13:18 -0400

    Changed to display proper error if osm install fails for any reason other than that it already exists

--- install-osm.sh
+++ install-osm.sh
@@ -6,15 +6,24 @@ OSM_VERSION=v0.11.0
 # Linux curl command only
 curl -sL "https://github.com/openservicemesh/osm/releases/download/$OSM_VERSION/osm-$OSM_VERSION-linux-amd64.tar.gz" | tar -vxzf -
 
-sudo mv ./linux-amd64/osm /usr/local/bin/osm
+mv ./linux-amd64/osm /usr/local/bin/osm
 osm version
 
 set +e
-OSM_INSTALL=$(osm install 2>&1) || true
+OSM_INSTALL=$(osm install 2>&1)
+exit_code=$?
 set -e
 
 echo $OSM_INSTALL
 
 if grep -q "Error: Mesh osm already exists" <<< "$OSM_INSTALL"; then
     osm mesh upgrade
+    exit 0
+fi
+
+# if the exit code is 1 otherwise, we should error out
+if (($exit_code > 0)); then
+    echo "Encountered some error in osm install, exiting"
+    echo "osm install error is: $OSM_INSTALL"
+    exit 1
 fi
